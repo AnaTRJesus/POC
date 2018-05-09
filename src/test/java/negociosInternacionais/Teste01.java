b661b5ea08cda64669fdc711b69ae0519de103bb
Ribeiro de Jesus <ana.ribeiro.de.jesus@accenture.com>
2018-05-09 18:11:09 -0300

    Criação do Projeto

---
+++ src/test/java/negociosInternacionais/Teste01.java
@@ -0,0 +1,36 @@
+package negociosInternacionais;
+
+
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+
+
+public class Teste01 {
+
+	public static WebDriver driver = null;
+
+
+	@Given ("Acessei a URL principal")
+	public void webCasasHome() {
+		
+		System.out.println("teste");
+		
+	}
+
+		
+
+
+
+
+	}
+	
+	
+
---
+++ src/test/java/negociosInternacionais/Teste02.java
@@ -0,0 +1,34 @@
+package negociosInternacionais;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Teste02 {
+	
+	public static WebDriver driver = null;
+
+	
+
+	@Given ("Testar link")
+	public void Teste() {
+		
+		System.out.println("teste");
+		
+
+	}
+
+	
+	
+
+	}
+	
+	
+
+
+
